4f47d7a31f822ef34ae18a2d639fcbb1df6956a7
Alexander Shpilko <shpilko.alexander@gmail.com>
2022-02-20 23:22:48 +0300

    Tamplates function, lessson 5, Task 6

--- Templates function/Templates_function/main.cpp
+++ Templates function/Templates_function/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <set>
-
+#include <map>
 
 using namespace std;
 
 
+
+template <typename Key, typename Value>
+ostream& operator<<(ostream& out, const pair<Key, Value> container){
+    out << "(" << container.first << ", " << container.second << ")";
+    return out;
+}
 
 template <typename Collection>
 ostream& Print(ostream& out, const Collection container){
@@ -38,10 +44,26 @@ ostream& operator<<(ostream& out, const set<Element> container){
     return out;
 }
 
+template <typename Key, typename Value>
+ostream& operator<<(ostream& out, const map<Key, Value> container){
+    out << "<";
+    Print(out, container);
+    out << ">";
+    return out;
+}
+
 int main() {
     const set<string> cats = {"Мурка"s, "Белка"s, "Георгий"s, "Рюрик"s};
     cout << cats << endl;
 
     const vector<int> ages = {10, 5, 2, 12};
     cout << ages << endl;
+
+    const map<string, int> cat_ages = {
+        {"Мурка"s, 10},
+        {"Белка"s, 5},
+        {"Георгий"s, 2},
+        {"Рюрик"s, 12}
+    };
+    cout << cat_ages << endl;
 }
