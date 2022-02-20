264711db39807ade752a8e514ec53ec8a82a13e4
Alexander Shpilko <shpilko.alexander@gmail.com>
2022-02-20 22:47:46 +0300

    Tamplates function, lessson 5, Task 5

--- Templates function/Templates_function/main.cpp
+++ Templates function/Templates_function/main.cpp
@@ -22,17 +22,26 @@ ostream& Print(ostream& out, const Collection container){
 
 template <typename Element>
 ostream& operator<<(ostream& out, const vector<Element> container){
+    out << "[";
     Print(out, container);
+    out << "]";
+
     return out;
 }
 
 template <typename Element>
 ostream& operator<<(ostream& out, const set<Element> container){
+    out << "{";
     Print(out, container);
+    out << "}";
+
     return out;
 }
 
 int main() {
+    const set<string> cats = {"Мурка"s, "Белка"s, "Георгий"s, "Рюрик"s};
+    cout << cats << endl;
+
     const vector<int> ages = {10, 5, 2, 12};
     cout << ages << endl;
 }
