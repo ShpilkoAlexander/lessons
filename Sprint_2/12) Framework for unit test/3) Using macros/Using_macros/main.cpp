6e65eee5544d1a12e86129167356dda060925e1a
Alexander Shpilko <shpilko.alexander@gmail.com>
2022-03-19 13:43:31 +0300

    Sprint_2, Topic_12, lesson_3

--- Sprint_2/12) Framework for unit test/3) Using macros/Using_macros/main.cpp
+++ Sprint_2/12) Framework for unit test/3) Using macros/Using_macros/main.cpp
@@ -1,9 +1,35 @@
+#include <cstdlib>
+#include <iomanip>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+void Assert(bool value, const string& str_func,
+            const string& file, const string& function,
+            const unsigned line,
+            const string& hint){
+    if(!value){
+        cout << file <<"("s << line << "): "s << function << ": "s;
+        cout << "ASSERT(" << str_func << ") failed."s;
+        if(!hint.empty()){
+            cout << " Hint: "s << hint;
+        }
+        cout << endl;
+        abort();
+    }
 }
+
+#define ASSERT(expr) Assert(expr, #expr, __FILE__, __FUNCTION__, __LINE__, ""s)
+/* реализовать самостоятельно */
+
+#define ASSERT_HINT(expr, hint) Assert(expr, #expr, __FILE__, __FUNCTION__, __LINE__, hint)
+/* реализовать самостоятельно */
+
+int main() {
+    string hello = "hello"s;
+    ASSERT(!hello.empty());
+    ASSERT_HINT(2 + 2 == 5, "This will fail"s);
+}
+
+
