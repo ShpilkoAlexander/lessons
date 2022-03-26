d89a3f92a59b466a67b132ae07a8dae7a8a4df51
Alexander Shpilko <shpilko.alexander@gmail.com>
2022-03-27 01:12:52 +0300

    Sprint_4, Topic_21, lesson 2, Task 2

--- Sprint_4/21) Stack, queue, dec/2) Stack/Stack/main.cpp
+++ Sprint_4/21) Stack, queue, dec/2) Stack/Stack/main.cpp
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <iostream>
+#include <numeric>
 #include <vector>
 
 using namespace std;
@@ -40,15 +42,83 @@ private:
     vector<Type> elements_;
 };
 
+// используем струкруту из элемента
+// и минимума в стеке до дна стека до текущего элемента включительно
+template <typename Type>
+struct PairWithMin {
+    Type element;
+    Type minimum;
+};
+
+template <typename Type>
+ostream& operator<<(ostream& out, const PairWithMin<Type>& pair_to_print) {
+    // будем выводить только сам элемент, выводить минимум не будем
+    out << pair_to_print.element;
+    return out;
+}
+
+template <typename Type>
+class StackMin {
+public:
+    void Push(const Type& new_element) {
+        // создадим структуру, которую будем класть в стек
+        PairWithMin<Type> new_pair = {new_element, new_element};
+        // проверим, что должно стать минимумом для текущего элемента -
+        // он сам, или предыдущий минимум
+        if (!elements_.IsEmpty() && new_pair.minimum > elements_.Peek().minimum) {
+            new_pair.minimum = elements_.Peek().minimum;
+        }
+        // положим структуру в стек
+        elements_.Push(new_pair);
+    }
+    void Pop() {
+        elements_.Pop();
+    }
+    const Type& Peek() const {
+        return elements_.Peek().element;
+    }
+    Type& Peek() {
+        return elements_.Peek().element;
+    }
+    void Print() const {
+        elements_.Print();
+    }
+    uint64_t Size() const {
+        return elements_.size();
+    }
+    bool IsEmpty() const {
+        return elements_.IsEmpty();
+    }
+    const Type& PeekMin() const {
+        return elements_.Peek().minimum;
+    }
+    Type& PeekMin() {
+        return elements_.Peek().minimum;
+    }
+
+private:
+    // используем в качестве элемента стека нашу структуру
+    Stack<PairWithMin<Type>> elements_;
+};
+
 int main() {
-    Stack<int> stack;
-    for (uint32_t i = 0; i < 10; ++i) {
-        stack.Push(i);
-        stack.Print();
+    StackMin<int> stack;
+    vector<int> values(5);
+
+    // заполняем вектор для тестирования нашего стека
+    iota(values.begin(), values.end(), 1);
+    // перемешиваем значения
+    random_shuffle(values.begin(), values.end());
+
+    // заполняем стек
+    for (int i = 0; i < 5; ++i) {
+        stack.Push(values[i]);
     }
+
+    // печатаем стек и его минимум, постепенно убирая из стека элементы
     while (!stack.IsEmpty()) {
-        stack.Pop();
         stack.Print();
+        cout << "Минимум = "s << stack.PeekMin() << endl;
+        stack.Pop();
     }
-    return 0;
 }
