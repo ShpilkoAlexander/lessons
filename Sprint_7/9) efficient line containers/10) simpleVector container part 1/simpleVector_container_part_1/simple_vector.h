3c8ff03b0cf2e9ad8dcfce3f9a60e3ab82485ad8
Alexander Shpilko <shpilko.alexander@gmail.com>
2022-05-18 11:49:43 +0300

    Sprint_7, Topic_9, lesson 10

---
+++ Sprint_7/9) efficient line containers/10) simpleVector container part 1/simpleVector_container_part_1/simple_vector.h
@@ -0,0 +1,149 @@
+#pragma once
+
+#include <cassert>
+#include <initializer_list>
+#include <algorithm>
+#include <stdexcept>
+#include <iostream>
+
+#include "array_ptr.h"
+
+using namespace std;
+
+template <typename Type>
+class SimpleVector {
+public:
+    using Iterator = Type*;
+    using ConstIterator = const Type*;
+
+    SimpleVector() noexcept = default;
+
+    // Создаёт вектор из size элементов, инициализированных значением по умолчанию
+    explicit SimpleVector(size_t size) : items_(size), size_(size), capacity_(size) {
+        Type t{};
+        fill(this->begin(), this->end(), t);
+    }
+
+    // Создаёт вектор из size элементов, инициализированных значением value
+    SimpleVector(size_t size, const Type& value) : items_(size), size_(size), capacity_(size) {
+        fill(this->begin(), this->end(), value);
+    }
+
+    // Создаёт вектор из std::initializer_list
+    SimpleVector(initializer_list<Type> init) : items_(init.size()), size_(init.size()), capacity_(init.size()) {
+        copy(init.begin(), init.end(), items_.Get());
+    }
+
+    // Возвращает количество элементов в массиве
+    size_t GetSize() const noexcept {
+        return size_;
+    }
+
+    // Возвращает вместимость массива
+    size_t GetCapacity() const noexcept {
+        return capacity_;
+    }
+
+    // Сообщает, пустой ли массив
+    bool IsEmpty() const noexcept {
+        return size_ == 0;
+    }
+
+    // Возвращает ссылку на элемент с индексом index
+    Type& operator[](size_t index) noexcept {
+        return items_[index];
+    }
+
+    // Возвращает константную ссылку на элемент с индексом index
+    const Type& operator[](size_t index) const noexcept {
+        return items_[index];
+    }
+
+    // Возвращает константную ссылку на элемент с индексом index
+    // Выбрасывает исключение std::out_of_range, если index >= size
+    Type& At(size_t index) {
+        if (index >= size_) {
+            throw out_of_range("Exceeding index"s);
+        }
+        return items_[index];
+    }
+
+    // Возвращает константную ссылку на элемент с индексом index
+    // Выбрасывает исключение std::out_of_range, если index >= size
+    const Type& At(size_t index) const {
+        if (index >= size_) {
+            throw out_of_range("Exceeding index"s);
+        }
+        return items_[index];
+    }
+
+    // Обнуляет размер массива, не изменяя его вместимость
+    void Clear() noexcept {
+        size_ = 0;
+    }
+
+    // Изменяет размер массива.
+    // При увеличении размера новые элементы получают значение по умолчанию для типа Type
+    void Resize(size_t new_size) {
+        // Напишите тело самостоятельно
+        Type t{};
+        if (new_size <= size_) {
+            size_ = new_size;
+        }
+        else if (new_size > size_ && new_size <= capacity_) {
+            fill(items_.Get() + size_, items_.Get() + new_size, t);
+            size_ = new_size;
+        }
+        else {
+            const size_t new_capacity = std::max(capacity_ * 2, new_size);
+            ArrayPtr<Type> extended_vector(new_capacity);
+            copy(items_.Get(), items_.Get() + size_, extended_vector.Get());
+            fill(extended_vector.Get() + size_, extended_vector.Get() + new_size, t);
+            items_.swap(extended_vector);
+            size_ = new_size;
+            capacity_ = new_capacity;
+        }
+    }
+
+    // Возвращает итератор на начало массива
+    // Для пустого массива может быть равен (или не равен) nullptr
+    Iterator begin() noexcept {
+        return &items_[0];
+    }
+
+    // Возвращает итератор на элемент, следующий за последним
+    // Для пустого массива может быть равен (или не равен) nullptr
+    Iterator end() noexcept {
+        return &items_[size_];
+    }
+
+    // Возвращает константный итератор на начало массива
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator begin() const noexcept {
+        return ConstIterator{&items_[0]};
+    }
+
+    // Возвращает итератор на элемент, следующий за последним
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator end() const noexcept {
+        return  ConstIterator{&items_[size_]};
+    }
+
+    // Возвращает константный итератор на начало массива
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator cbegin() const noexcept {
+        return begin();
+    }
+
+    // Возвращает итератор на элемент, следующий за последним
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator cend() const noexcept {
+        return end();
+    }
+
+private:
+    ArrayPtr<Type> items_;
+
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+};
