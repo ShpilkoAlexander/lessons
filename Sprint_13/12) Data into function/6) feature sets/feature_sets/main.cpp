200db3cbc895cb5dd03eb9a2efab91455b187347
ShpilkoAlexander <shpilko.alexander@gmail.com>
2023-08-29 21:36:14 +0300

    Sprint_13, Topic_12, lesson 6

---
+++ Sprint_13/12) Data into function/6) feature sets/feature_sets/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <list>
+#include <set>
+#include <string_view>
+#include <vector>
+
+using namespace std;
+
+// Измените сигнатуру этой функции, чтобы она могла
+// сливать не только векторы, но и любые другие контейнеры
+template <typename InputIt1, typename InputIt2>
+void Merge(InputIt1 first_begin, InputIt1 first_end, InputIt2 second_begin, InputIt2 second_end, ostream& out) {
+    while (first_begin != first_end && second_begin != second_end) {
+        if (less<>()(*first_begin , *second_begin)) {
+            out << *(first_begin++) << endl;
+        } else {
+            out << *(second_begin++)  << endl;
+        }
+    }
+    while (first_begin != first_end ) {
+        out << *(first_begin++) << endl;
+    }
+    while (second_begin != second_end) {
+        out << *(second_begin++) << endl;
+    }
+}
+
+// Реализуйте эти функции. Они должны вызывать ваш вариант функции Merge.
+// Чтобы сократить кличество работы, можете реализовать вместо них одну шаблонную.
+template <typename T, typename S>
+void MergeSomething(const T& src1, const S& src2, ostream& out) {
+    Merge(src1.begin(), src1.end(), src2.begin(), src2.end(), out);
+}
+
+// Реализуйте эту функцию:
+template <typename T>
+void MergeHalves(const vector<T>& src, ostream& out) {
+    size_t mid = (src.size() + 1) / 2;
+    // Сделать Merge участка вектора от 0 до mid и от mid до конца.
+    // Элемент с индексом mid включается во второй диапазон.
+    Merge(src.begin(), src.begin() + mid, src.begin() + mid, src.end(), out);
+}
+
+int main() {
+    vector<int> v1{60, 70, 80, 90};
+    vector<int> v2{65, 75, 85, 95};
+    vector<int> combined{60, 70, 80, 90, 65, 75, 85, 95};
+    list<double> my_list{0.1, 72.5, 82.11, 1e+30};
+    string_view my_string = "ACNZ"sv;
+    set<unsigned> my_set{20u, 77u, 81u};
+
+    // пока функция MergeSomething реализована пока только для векторов
+    cout << "Merging vectors:"sv << endl;
+    MergeSomething(v1, v2, cout);
+
+    cout << "Merging vector and list:"sv << endl;
+    MergeSomething(v1, my_list, cout);
+
+    cout << "Merging string and list:"sv << endl;
+    MergeSomething(my_string, my_list, cout);
+
+    cout << "Merging set and vector:"sv << endl;
+    MergeSomething(my_set, v2, cout);
+
+    cout << "Merging vector halves:"sv << endl;
+    MergeHalves(combined, cout);
+}
